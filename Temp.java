099d206dd5b7a70aab72028ef1ee6944c14cd406
Shrinath-11 <145825284+Shrinath-11@users.noreply.github.com>
2023-11-24 12:08:51 +0530

    Update Temp.java

--- Temp.java
+++ Temp.java
@@ -4,5 +4,8 @@ public class Temp {
     public static void main(String[] args) {
         System.out.println("Hello Earth!");
         System.out.println("Expleo Pune");
+        int i = 10;
+        int b = 30;
+         System.out.println(i+b);
     }
 }
